4340b8d00a6b6ce47232601ff14c55163ba2f396
Lokesh Garg <lokeshgarg0511@gmail.com>
2026-08-12 11:26:54 +0530

    first commit

---
+++ lecture-3/script.js
@@ -0,0 +1,12 @@
+// console.log("hello")
+const math = require("./math.js");
+const express=require("express");
+const os=require("os");
+//console.log(add(2, 3));
+// console.log(math.add(2, 3));
+// console.log(math.subtract(5,3));
+// console.log("hello world!");
+
+console.log(os.platform())
+console.log(os.cpus());
+console.log(os.totalmem()/(1024*1024),"GB");
